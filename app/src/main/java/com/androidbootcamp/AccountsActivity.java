d3e8b3a626a7584124fbbd33cdb331b889057733
jhansireddy <jhansireddy007@gmail.com>
2016-01-15 16:01:27 +0530

    Added Fragment

--- app/src/main/java/com/androidbootcamp/AccountsActivity.java
+++ app/src/main/java/com/androidbootcamp/AccountsActivity.java
@@ -1,6 +1,7 @@
 package com.androidbootcamp;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -16,47 +17,22 @@ import com.androidbootcamp.util.CameraUtility;
  */
 public class AccountsActivity extends Activity {
 
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 12;
-    public static String UserName = "USER_NAME";
-    private ImageView profilePic;
+    public static String UserName ="userName";
+    private AccountsFragment accountsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounts);
-        init();
-    }
-
-    private void init() {
-        TextView welcomeTextView = (TextView) findViewById(R.id.welcomeTextView);
-        welcomeTextView.setText(getWelcomeText());
-        profilePic = (ImageView) findViewById(R.id.profilePic);
-        profilePic.setOnClickListener(new ProfilePicClickListener());
-    }
-
-    private String getWelcomeText() {
-        String userName = getIntent().getStringExtra(UserName);
-        return getString(R.string.welcome) + " " + userName;
-    }
-
-    private class ProfilePicClickListener implements View.OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            captureImageFromCamera();
-        }
-    }
-
-    private void captureImageFromCamera() {
-        CameraUtility.captureImage(this, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        accountsFragment = (AccountsFragment) getFragmentManager().findFragmentById(R.id.accountsFragment);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+        if (requestCode == AccountsFragment.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             Bitmap bitmap = CameraUtility.extractImage(data);
-            profilePic.setImageBitmap(bitmap);
+            accountsFragment.setImageBitmap(bitmap);
         }
     }
 }
---
+++ app/src/main/java/com/androidbootcamp/AccountsFragment.java
@@ -0,0 +1,61 @@
+package com.androidbootcamp;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.androidbootcamp.util.CameraUtility;
+
+/**
+ * Created by jhansirk on 1/15/16.
+ */
+public class AccountsFragment extends Fragment {
+
+    private View view;
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 12;
+    private ImageView profilePic;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_accounts, null);
+        init();
+        return view;
+    }
+
+    private void init() {
+        TextView welcomeTextView = (TextView) view.findViewById(R.id.welcomeTextView);
+        profilePic = (ImageView) view.findViewById(R.id.profilePic);
+        profilePic.setOnClickListener(new ProfilePicClickListener());
+    }
+
+    private String getWelcomeText() {
+        String userName = getActivity().getIntent().getStringExtra(AccountsActivity.UserName);
+        return getString(R.string.welcome) + " " + userName;
+    }
+
+    public void setImageBitmap(Bitmap bitmap) {
+        profilePic.setImageBitmap(bitmap);
+    }
+
+    private class ProfilePicClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            captureImageFromCamera();
+        }
+    }
+
+    private void captureImageFromCamera() {
+        CameraUtility.captureImage(getActivity(), CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+}
